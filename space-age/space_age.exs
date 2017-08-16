c6f6c6c255bf0e9f87bdb16357ee081d40046d56
Spencer Alan <spencer.alan@icloud.com>
2017-08-15 22:08:05 -0700

    space age tests passing

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -6,8 +6,22 @@ defmodule SpaceAge do
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
+
+  @orbital_periods %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  }
+
+  @seconds_per_year 31557600
+
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    seconds / (Map.get(@orbital_periods, planet) * @seconds_per_year)
   end
 end
