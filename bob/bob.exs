23918c2ff3e24e719f3e46ed5229fe27f2b4dcd5
Spencer Alan <spencer.alan@icloud.com>
2017-11-11 22:50:52 -0800

    tests passing

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,29 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      question?(input) ->
+        "Sure."
+      silence?(input) ->
+        "Fine. Be that way!"
+      yelling?(input) ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
+  end
+
+  def question?(input) do
+    String.ends_with?(input, "?")
+  end
+  
+  def yelling?(input) do
+    Regex.match?(~r/\p{L}/, input) &&
+    String.upcase(input)
+    |> String.equivalent?(input)
+  end
+
+  def silence?(input) do
+    String.trim(input)
+    |> String.length() == 0
   end
 end
