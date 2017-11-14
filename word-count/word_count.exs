10c2b9963c86ddedde62a6c75dbaf7394fa0adf2
Spencer Alan <spencer.alan@icloud.com>
2017-11-13 17:24:22 -0800

    tests passing for word count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,23 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    parse(sentence)
+    |> Enum.reduce(%{}, fn(x, acc) ->
+      if Map.has_key?(acc, x) do
+        Map.replace(acc, x, acc[x] + 1)
+      else
+        Map.merge(acc, %{x => 1})
+      end
+    end)
+  end
 
+  def parse(sentence) do
+    downcased_string = String.downcase(sentence)
+    Regex.scan(~r/
+      [[:alnum:]]+
+      -*
+      [[:alpha:]]*
+      /ux, downcased_string)
+    |> List.flatten
   end
 end
